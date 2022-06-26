8f489432dc10d261e2395d2583c96ea90aa48d55
weilee0506 <weilee19960506@gmail.com>
2022-06-26 11:13:12 +0800

    0626 commit-finish functionality

--- checkmypass.py
+++ checkmypass.py
@@ -3,6 +3,7 @@
 # 可以讓我們發送request
 import requests
 import hashlib
+import sys
 
 # api使用是用hash
 # 也使用了k-anonymity k匿名性，只收密碼的hash值的前5碼(底下的cbfda)
@@ -17,7 +18,7 @@ def request_api_data(query_char):
     # 取得含有前5碼的所有hash
     url = "https://api.pwnedpasswords.com/range/" + query_char
     res = requests.get(url)
-    print(res)
+    # print(res)
     # print(F"Error fetching: {res.status_code}, check the api and try again")
 
     if res.status_code != 200:
@@ -27,23 +28,60 @@ def request_api_data(query_char):
     return res
 
 
+def read_res(response):
+    # 可以去讀response的值
+    # api的response是取得前五碼一樣的後面剩下部分和被hacked的數量 (e.g.007F6D133ECAC7376F0868146879AC82118:4)
+    print(response.text)
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    # 把api response得到的hashes和count分開
+    hashes = (line.split(":") for line in hashes.text.splitlines())
+    for h, count in hashes:
+        # 看我要測的hash_to_check在response裡面有沒有，有的話return count
+        if h == hash_to_check:
+            return count
+    return 0
+
+
 def pwned_api_check(password):
-    # 把password跟api reponse做對照
+    # 把password跟api response做對照
 
     # 在hash之前要先encode成utf-8的格式
-    print(password.encode("utf-8"))
+    # print(password.encode("utf-8"))
     # hash object
-    print(hashlib.sha1(password.encode("utf-8")))
+    # print(hashlib.sha1(password.encode("utf-8")))
     # hash object轉乘hexadecimal string十六進位
-    print(hashlib.sha1(password.encode("utf-8")).hexdigest())
+    # print(hashlib.sha1(password.encode("utf-8")).hexdigest())
     # 變大寫，才能跟api response做對應
-    print(hashlib.sha1(password.encode("utf-8")).hexdigest().upper())
+    # print(hashlib.sha1(password.encode("utf-8")).hexdigest().upper())
 
     sha1_password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
+    # 把前5個跟後面存成變數
+    first5_char, tail = sha1_password[:5], sha1_password[5:]
+    response = request_api_data(first5_char)
     # print(sha1_password)
-    return sha1_password
+    # print("----------")
+    # print(first5_char)
+    # print(tail)
+    # print("----------")
+    # print(response)
+    return get_password_leaks_count(response, tail)
 
 
-request_api_data("cbfda")
+def main(args):
+    # args接收要測的密碼
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f"{password} was found {count} times!! you should change it!!")
+        else:
+            print(f"{password} was not found!! Carry on!!")
+    return "done!!"
 
-pwned_api_check("zzz")
+
+# 這個file只有在他是main時才會執行
+if __name__ == "__main__":
+    # 要測幾個密碼都可以
+    # sys.exit確保執行完會從這個file exit，回到command line，done!!會出現
+    sys.exit(main(sys.argv[1:]))
